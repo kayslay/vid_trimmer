099776ff4028317f4e4d50770041e39cb06423ae
badewa kayode <badewakayode1@gmail.com>
2020-06-14 13:03:23 +0100

    return a better error for youtube panic

--- internal/input/youtube.go
+++ internal/input/youtube.go
@@ -2,9 +2,13 @@ package input
 
 import (
 	"context"
+	"fmt"
+	errors2 "github.com/bushaHQ/httputil/errors"
 	yt "github.com/kkdai/youtube"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"gitlab.com/kayslay/vid_trimmer/config"
+	"net/http"
 )
 
 type youtube struct {
@@ -16,7 +20,20 @@ func NewYoutube(dir string, url Interface) *youtube {
 	return &youtube{dir: dir, urlInput: url}
 }
 
-func (y youtube) Fetch(ctx context.Context, p string) (string, error) {
+func (y youtube) Fetch(ctx context.Context, p string) (str string, err error) {
+
+	defer func() {
+		if rvr := recover(); rvr != nil {
+			err = errors2.CoverErr(
+				fmt.Errorf("%v", rvr),
+				errors2.New("Can not trim this video file. working on a fix", http.StatusServiceUnavailable),
+				log.WithFields(log.Fields{
+					"context": "defer",
+					"method":  "youtube/fetch",
+				}),
+			)
+		}
+	}()
 
 	dl := yt.NewYoutube(viper.GetString(config.Env) != "production")
 	if err := dl.DecodeURL(p); err != nil {
